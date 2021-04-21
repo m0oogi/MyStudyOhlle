d611d7d408b11e90caa170fc6a31c97208719143
mooogi <sm01270913@gmail.com>
2021-04-21 16:28:23 +0900

    fixed untracked files

--- src/main/java/com/mystudyohlle/mystudyohlle/App.java
+++ src/main/java/com/mystudyohlle/mystudyohlle/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MystudyohlleApplication {
+public class App {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MystudyohlleApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 
 }
