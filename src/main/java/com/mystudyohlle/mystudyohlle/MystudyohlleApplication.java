c56ff9f2c64e8c97889f3c25ef5eb8e687dab15e
mooogi <sm01270913@gmail.com>
2021-04-21 16:01:34 +0900

    first commit

---
+++ src/main/java/com/mystudyohlle/mystudyohlle/MystudyohlleApplication.java
@@ -0,0 +1,13 @@
+package com.mystudyohlle.mystudyohlle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystudyohlleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MystudyohlleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mystudyohlle/mystudyohlle/MystudyohlleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystudyohlle.mystudyohlle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystudyohlleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
